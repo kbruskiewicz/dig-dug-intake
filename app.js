97dffed4e8132715519eddcc98eefc3fbb3d35a0
Kenneth <kenneth.bruskiewicz@gmail.com>
2021-04-27 17:06:37 -0700

    column formatting

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const config = require("./config");
 
 const { Sequelize, DataTypes, Model, UniqueConstraintError } = require('sequelize');
 
-const express = require("express");
-
 const crypto = require('crypto');
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const GoogleStrategy = require( 'passport-google-oauth20' ).Strategy;
 const flash = require("connect-flash");
+
+const nodemailer = require("nodemailer");
+
+const express = require("express");
 
 const loadedConfig = config.loadConfig();
 
@@ -278,7 +280,40 @@ async function registerDataset({ accession_id, user_id, name, institution, descr
 
 // initialize resources
 try {
-    sequelize.sync().then(async () => {
+    sequelize.sync()
+    .then(async () => {
+        // Generate test SMTP service account from ethereal.email
+        // Only needed if you don't have a real mail account for testing
+        let testAccount = await nodemailer.createTestAccount();
+
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+            user: testAccount.user, // generated ethereal user
+            pass: testAccount.pass, // generated ethereal password
+            },
+        });
+
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: '"Fred Foo 👻" <foo@example.com>', // sender address
+            to: "bar@example.com, baz@example.com", // list of receivers
+            subject: "Hello ✔", // Subject line
+            text: "Hello world?", // plain text body
+            html: "<b>Hello world?</b>", // html body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+        // Preview only available when sending through an Ethereal account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    })
+    .then(async () => {
         
         const username = loadedConfig.test.username;
         const password = loadedConfig.test.password;
